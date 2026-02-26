fa72620dc4cb4aaf0e46ba231303770a8d0d2dda
aadennis <dennis.circ@gmail.com>
2026-02-26 18:57:03 +0000

    reverse it all?

--- VoiceVideoSync/stitch_voice_video.sh
+++ VoiceVideoSync/stitch_voice_video.sh
@@ -6,12 +6,11 @@ dir='/mnt/c/temp/demo/'
 sound_file=${dir}/x2_audio.mp4
 video_file=${dir}/x2.mp4
 output_file=${dir}/x_synced.mp4
-SECONDS=+18.00
+SECONDS=16.0   # audio starts 16 seconds after video
 
 ffmpeg -y \
-  -i "$sound_file" \
-  -itsoffset "$SECONDS" -i "$video_file" \
-  -map 0:a -map -0:v -map -1:a -map 1:v \
+  -i "$video_file" \
+  -itsoffset "$SECONDS" -i "$sound_file" \
+  -map 0:v -map -0:a -map 1:a \
   -c:v copy -c:a aac -b:a 192k \
   "$output_file"
-
