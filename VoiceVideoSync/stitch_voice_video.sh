afb9efa1a5dcd7a1c7378cadbbb4f87d81afecbf
aadennis <dennis.circ@gmail.com>
2026-02-26 19:08:43 +0000

    From playing, turns out that SECONDS is a reserved constant. Should have guessed that.

--- VoiceVideoSync/stitch_voice_video.sh
+++ VoiceVideoSync/stitch_voice_video.sh
@@ -5,14 +5,14 @@ set -euo pipefail
 dir='/mnt/c/temp/demo/'
 sound_file=${dir}/x2_audio.mp4
 video_file=${dir}/x2.mp4
-output_file=${dir}/x_synced.mp4
-SECONDS=16.0   # audio starts 16 seconds after video
+output_file=${dir}/x_synced3.mp4
+OFFSET=16.0   # audio starts 16 seconds after video
 
 ffmpeg -y \
-  -i /mnt/c/temp/demo/x2.mp4 \
-  -itsoffset 16.0 -i /mnt/c/temp/demo/x2_audio.mp4 \
+  -i "${video_file}" \
+  -itsoffset "${OFFSET}" -i "${sound_file}" \
   -map 0:v -map -0:a -map 1:a \
   -c:v copy -c:a aac -b:a 192k \
-  /mnt/c/temp/demo/x_synced.mp4
+  "${output_file}"
 
-  
+
