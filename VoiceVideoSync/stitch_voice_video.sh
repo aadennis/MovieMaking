924e71310c39157c7c0516db5afe07158c419a1c
aadennis <dennis.circ@gmail.com>
2026-02-26 18:45:23 +0000

    er

--- VoiceVideoSync/stitch_voice_video.sh
+++ VoiceVideoSync/stitch_voice_video.sh
@@ -6,10 +6,12 @@ dir='/mnt/c/temp/demo/'
 sound_file=${dir}/x2_audio.mp4
 video_file=${dir}/x2.mp4
 output_file=${dir}/x_synced.mp4
-SECONDS=10.00
+SECONDS=+18.00
 
-ffmpeg -y -i "$video_file" -itsoffset "$SECONDS" -i "$sound_file" \
- -map 0:v -map -0:a -map 1:a \
+ffmpeg -y \
+  -i "$sound_file" \
+  -itsoffset "$SECONDS" -i "$video_file" \
+  -map 0:a -map -0:v -map -1:a -map 1:v \
   -c:v copy -c:a aac -b:a 192k \
-  -shortest \
   "$output_file"
+
